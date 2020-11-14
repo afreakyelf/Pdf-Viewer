df77257644908d62c109399161b2e5096f26edc0
afreakyelf <i.rajat.mittal@redcarpet.com>
2020-11-14 16:46:59 +0530

    bug fixed

--- pdfViewer/src/main/java/com/rajat/pdfviewer/PdfViewerActivity.kt
+++ pdfViewer/src/main/java/com/rajat/pdfviewer/PdfViewerActivity.kt
@@ -67,6 +67,7 @@ class PdfViewerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pdf_viewer)
 
         setUpToolbar(
             intent.extras!!.getString(
