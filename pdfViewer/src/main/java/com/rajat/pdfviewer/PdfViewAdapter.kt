0225f7d2fc62b1f33da5bd00f4bf6d66b41316c6
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2022-12-01 19:53:17 +0330

    Add spacing for pages like the android margin

--- pdfViewer/src/main/java/com/rajat/pdfviewer/PdfViewAdapter.kt
+++ pdfViewer/src/main/java/com/rajat/pdfviewer/PdfViewAdapter.kt
@@ -1,23 +1,32 @@
 package com.rajat.pdfviewer
 
+import android.view.View
+import android.graphics.Rect
+import android.view.ViewGroup
 import android.graphics.Bitmap
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.rajat.pdfviewer.util.hide
+import com.rajat.pdfviewer.util.show
 import android.view.animation.AlphaAnimation
+import androidx.core.view.updateLayoutParams
 import android.view.animation.LinearInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_item_pdf_page.view.*
+import kotlinx.android.synthetic.main.pdf_view_page_loading_layout.view.*
 
 /**
  * Created by Rajat on 11,July,2020
  */
 
-internal class PdfViewAdapter(private val renderer: PdfRendererCore) :
+internal class PdfViewAdapter(
+    private val renderer: PdfRendererCore,
+    private val pageSpacing: Rect,
+    private val enableLoadingForPages: Boolean
+) :
     RecyclerView.Adapter<PdfViewAdapter.PdfPageViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PdfPageViewHolder {
-        val v =
-            LayoutInflater.from(parent.context).inflate(R.layout.list_item_pdf_page, parent, false)
+        val v = LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_item_pdf_page, parent, false)
         return PdfPageViewHolder(v)
     }
 
@@ -26,20 +35,24 @@ internal class PdfViewAdapter(private val renderer: PdfRendererCore) :
     }
 
     override fun onBindViewHolder(holder: PdfPageViewHolder, position: Int) {
-        holder.bind()
+        holder.bind(position)
     }
 
     inner class PdfPageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind() {
+        fun bind(position: Int) {
             with(itemView) {
                 pageView.setImageBitmap(null)
-                renderer.renderPage(adapterPosition) { bitmap: Bitmap?, pageNo: Int ->
-                    if (pageNo != adapterPosition)
+                renderer.renderPage(position) { bitmap: Bitmap?, pageNo: Int ->
+                    if (pageNo != position)
                         return@renderPage
                     bitmap?.let {
-                        pageView.layoutParams = pageView.layoutParams.apply {
+                        pageView.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                             height =
                                 (pageView.width.toFloat() / ((bitmap.width.toFloat() / bitmap.height.toFloat()))).toInt()
+                            this.topMargin = pageSpacing.top
+                            this.leftMargin = pageSpacing.left
+                            this.rightMargin = pageSpacing.right
+                            this.bottomMargin = pageSpacing.bottom
                         }
                         pageView.setImageBitmap(bitmap)
                         pageView.animation = AlphaAnimation(0F, 1F).apply {
