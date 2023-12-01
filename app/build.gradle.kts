2d9f92c36268963e074d4dac439b2676b706cac0
Rajat Mittal <rjmittal07@gmail.com>
2023-11-30 18:45:20 -0800

    Multidex version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.android.support:multidex:2.0.0")
+    implementation("androidx.multidex:multidex:2.0.1")
     implementation(project(":pdfViewer"))
 
     testImplementation("androidx.test:core:1.5.0")
