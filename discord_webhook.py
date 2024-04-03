3410111e9071729d94fb8f31136afb8111b6062a
Rajat Mittal <38572147+afreakyelf@users.noreply.github.com>
2024-04-02 21:45:22 -0700

    Update discord_webhook.py

--- discord_webhook.py
+++ discord_webhook.py
@@ -11,7 +11,7 @@ def main():
     release_url = os.getenv('RELEASE_URL')
 
     # Construct the message content with Markdown
-    content = f"Hello everyone, **New Release: {release_tag}** \n\n{release_body} \n\n[View Release]({release_url})"
+    content = f"Hello everyone, \n**🚀New Release: {release_tag}🚀** \n{release_body} \n[View Release]({release_url})"
     
     # Prepare the webhook payload
     payload = {
