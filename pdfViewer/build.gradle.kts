f554c21642b0ef5e923c299ea0820aef46b6ad85
Laurentiu Cazalbasu <>
2025-03-24 21:53:39 +0200

    Complete the fix for 180 issue by updating the checks for the Android version. This issue also happens on API version 34

--- pdfViewer/build.gradle.kts
+++ pdfViewer/build.gradle.kts
@@ -95,7 +95,7 @@ mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
 
-    coordinates("io.github.afreakyelf", "Pdf-Viewer", "2.3.0")
+    coordinates("io.github.afreakyelf", "Pdf-Viewer", "2.3.1")
 
     pom {
         name.set("PDF Viewer")
